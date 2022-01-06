117b0475becfc54f37c4f1943044e98b84311928
valeriocipolla <valeriocipolla92@gmail.com>
2022-01-06 14:15:07 +0000

    task 3 completed

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,15 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import './index.css';
+import "./index.css";
 
-import samplePosts from '../../libs/data';
+import samplePosts from "../../libs/data";
 
-import Post from '../Post';
+import Post from "../Post";
 
 function Main() {
   const [posts, setPosts] = useState(samplePosts);
 
-  return <main id="main"></main>;
+  return (
+    <main id="main">
+      {samplePosts.map(function (post) {
+        return (
+          <Post
+            key={post.postId}
+            title={post.title}
+            date={post.date}
+            author={post.author}
+            text={post.text}
+            highlights={post.highlights}
+            image={post.image}
+          />
+        );
+      })}
+    </main>
+  );
 }
 
 export default Main;
